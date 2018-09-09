2b852d7c2183cdc2932fa47b4d0997e1b324dda7
bmmay0 <bmmay0@CC270-86274.frostburg.edu>
2018-09-09 19:58:45 -0400

    Created file using correct naming scheme

---
+++ COSC310_ASN/src/edu/frostburg/cosc310/lab00/MayBryan00.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.frostburg.cosc310.lab00;
+
+/**
+ *
+ * @author bmmay0
+ */
+public class MayBryan00 {
+    
+}
