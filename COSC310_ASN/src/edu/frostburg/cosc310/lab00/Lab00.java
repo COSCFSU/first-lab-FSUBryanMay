3cee44fd5dcab43cea2297c177309174a2ae72c1
bmmay0 <bmmay0@CC270-86274.frostburg.edu>
2018-09-09 20:20:09 -0400

    Added parameter to problem2

--- COSC310_ASN/src/edu/frostburg/cosc310/lab00/Lab00.java
+++ COSC310_ASN/src/edu/frostburg/cosc310/lab00/Lab00.java
@@ -1,15 +1,12 @@
 /*
     This is the interface for Lab00 in COSC310.  Create a class that implements
         this interface in the same package and call it LnameFnameLab00.
-
     The purpose of this lab is primarily to get used to coding again, but it
         will also help us to begin using Github.  All of our code will
         be committed to Github Classroom, so please familiarize yourself with it.
-
     If you do not have a Github account, please create one using the link
         provided.  If you do have an account already, you will need to tie it
         to this classroom account.
-
     !! The general process of what you should be doing:
         Whenever you have a "working version" of your code, COMMIT it.  This
         saves a LOCAL copy (to your hard drive).  Every now and then you should
@@ -30,5 +27,5 @@ public interface Lab00 {
     
     public long problem0(int size);
     public long problem1(int size);
-    public long problem2();
+    public long problem2(int size);
 }
