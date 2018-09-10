342a0141c152e90af305219e3258ee10a13f0ec1
bmmay0 <bmmay0@CC270-86274.frostburg.edu>
2018-09-09 20:03:45 -0400

    Copied problem0 + problem1 from KennedySteveLab00 to meet interface req

--- COSC310_ASN/src/edu/frostburg/cosc310/lab00/MayBryan00.java
+++ COSC310_ASN/src/edu/frostburg/cosc310/lab00/MayBryan00.java
@@ -10,6 +10,42 @@ package edu.frostburg.cosc310.lab00;
  * @author Bryan May
  * @version 2018.09.09
  */
-public class MayBryan00 {
+public class MayBryan00 implements Lab00{
     
+    /**
+     * Makes a long string using Java concatenation.
+     * @param size the # of times
+     * @return how long it took
+     */
+    @Override
+    public long problem0(int size) {
+        System.out.println("This is just an example, but please follow this template.");
+        String s = "";
+        long start = System.currentTimeMillis();
+
+        for (int i=0; i<size; i++) {
+            s = s + i;
+        }
+        
+        long end = System.currentTimeMillis();
+        return end - start;
+    }
+
+    /**
+     * Makes a long string using Java StringBuilder
+     * @param size the # of times
+     * @return how long it took
+     */
+    @Override
+    public long problem1(int size) {
+        StringBuilder sb = new StringBuilder();
+        long start = System.currentTimeMillis();
+
+        for (int i=0; i<size; i++) {
+            sb.append(i);
+        }
+        
+        long end = System.currentTimeMillis();
+        return end - start;
+    }
 }
